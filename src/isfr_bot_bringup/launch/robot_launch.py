a74902921d98bd1f910bdeb8c636a9dc2c559147
Ebbe Wertz <ebbe.wertz8@gmail.com>
2026-01-06 15:31:42 +0100

    config files gemaakt: map = copied van vorige versie, nav2 is copied van installed package specifiek voor turtlebot waffle, ros_control is gemaakt uit info van de urdf files

--- src/isfr_bot_bringup/launch/robot_launch.py
+++ src/isfr_bot_bringup/launch/robot_launch.py
@@ -4,17 +4,52 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    
-    # maak path naar de webots world file in de bringup package
+    # Path to the bringup package
     pkg_dir = get_package_share_directory('isfr_bot_bringup')
-    world_file = os.path.join(pkg_dir, 'worlds/my_custom_world.wbt')
 
-    # launch de world in webots via de webots_ros2_driver
+    # Webots world file
+    world_file = os.path.join(pkg_dir, 'worlds', 'my_custom_world.wbt')
+
+    # ROS 2 control YAML
+    ros2_control_file = os.path.join(pkg_dir, 'config', 'ros2_control.yaml')
+
     return LaunchDescription([
+        # Launch Webots with your world
         Node(
             package='webots_ros2_driver',
             executable='driver',
             output='screen',
-            parameters=[{'world': world_file}],
-        )
+            parameters=[{
+                'world': world_file,
+                'use_sim_time': True
+            }],
+        ),
+
+        # Start ROS 2 control node with controllers
+        Node(
+            package='controller_manager',
+            executable='ros2_control_node',
+            output='screen',
+            parameters=[ros2_control_file, {'use_sim_time': True}],
+        ),
+
+        # Spawn controllers automatically
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_state_broadcaster'],
+            output='screen',
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['diffdrive_controller'],
+            output='screen',
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['arm_controller'],
+            output='screen',
+        ),
     ])
