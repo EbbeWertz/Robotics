f0395c6c26e7bd5c449e0fa7b4b74007ca8e951b
Ebbe Wertz <ebbe.wertz8@gmail.com>
2026-01-06 16:21:16 +0100

    build files gefixt

--- src/isfr_bot_description/setup.py
+++ src/isfr_bot_description/setup.py
@@ -7,9 +7,16 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/protos', [
+            'protos/openmanipulator_x.proto',
+            'protos/turtlebot3_waffle.proto'
+        ]),
+        ('share/' + package_name + '/urdf', [
+            'urdf/openmanipulator_x.urdf',
+            'urdf/turtlebot3_waffle.urdf'
+        ])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
--- src/isfr_bot_moveit_config/setup.py
+++ src/isfr_bot_moveit_config/setup.py
@@ -7,10 +7,20 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch', [
+        'launch/moveit_launch.py',
+        'launch/open_manipulator_x_moveit.launch.py'
+    ]),
+    ('share/' + package_name + '/config', [
+        'config/kinematics.yaml',
+        'config/chomp_planning.yaml',
+        'config/ompl_planning.yaml',
+        'config/pilz_cartesian_limits.yaml',
+        'config/pilz_industrial_motion_planner_planning.yaml',
+        'config/moveit.rviz'
+    ])],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ebbew',
