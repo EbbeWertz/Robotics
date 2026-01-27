a27302a446f369c3b94a53eacad0d8bf255a4317
RobinLambregts <robin.lambregts@student.uhasselt.be>
2026-01-27 20:23:59 +0100

    knowledge aan fetch toegevoegd

--- src/isfr_bot_bringup/launch/object_fetch.launch.py
+++ src/isfr_bot_bringup/launch/object_fetch.launch.py
@@ -15,6 +15,8 @@ from isfr_bot_webots.launcher_scaffold import generate_webots_launch_description
 
 def generate_launch_description():
 
+    map_file = os.path.join(get_package_share_directory('isfr_bot_nav'),'config', 'map.yaml')
+
     webots_controllers_launcher = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('isfr_bot_webots'), 'launch', 'controllers.launch.py')
@@ -36,12 +38,24 @@ def generate_launch_description():
     vision_launcher = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('isfr_bot_vision'), 'launch', 'yolo.launch.py')
-        )
+        ),
+        launch_arguments=[
+            ('map', map_file),
+        ]
+    )
+    
+    knowledge_launcher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('isfr_bot_knowledge'), 'launch', 'knowledge_map.launch.py')
+        ),
+        launch_arguments=[
+            ('map', map_file),
+        ]
     )
 
     return generate_webots_launch_description(
         start_immediately_nodes = [],
-        start_after_webots_init_nodes = [webots_controllers_launcher, nav_launcher, fetch_skill_launcher, vision_launcher],
+        start_after_webots_init_nodes = [webots_controllers_launcher, nav_launcher, fetch_skill_launcher, vision_launcher, knowledge_launcher],
         controller_remappings = [('/diffdrive_controller/cmd_vel', '/cmd_vel_stamped')]
     )
     
